525ce697f1708f027be424c3eb534da74963234d
guoxinming <guoxinming@zbj.com>
2021-01-19 19:54:18 +0800

    手写JSON.stringify

---
+++ JS/手写JSON.stringify/stringify.js
@@ -0,0 +1,34 @@
+/**
+ * 实现JSON.stringify
+ * @param {String/Number/Boolean/Null/Object} data
+ */
+function stringify(data) {
+  let type = typeof data;
+
+  if(data === null) {
+    return 'null';
+  }else if(type === 'number') {// NaN/Infinity/-Infinity返回'null'
+    // isFinite用于检测参数是否是无穷大(false:参数是 NaN，正无穷大或者负无穷大)
+    return isFinite(data) ? data.toString() : 'null';
+  }else if(type === 'boolean') {
+    return data.toString();
+  }else if(type === 'string') {
+    return '"' + data + '"';
+  }else if(type === 'object') {
+    if(data instanceof Array) {// 数组
+      let res = '[';
+      for (var i = 0; i < data.length; i++){
+        res += (i ? ', ' : '') + stringify(data[i]);
+      }  
+      return res+']';
+    }else if(Object.prototype.toString.call(data) === '[object Object]') {// 对象
+      let tmp = [];
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {// 判断对象自身属性
+          tmp.push(stringify(key)+':'+stringify(data[key]));
+        }
+      }
+      return '{'+tmp.join()+'}';
+    }
+  }
+}
---
+++ JS/手写instanceOf/instanceOf.js
@@ -0,0 +1,9 @@
+/**
+ * 实现instanceof功能
+ *
+ * @param {*} left
+ * @param {*} right
+ */
+function instanceOf(left,right) {
+
+}
