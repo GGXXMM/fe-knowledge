d8d6c3fe07875f4d3e0a504798a1022858c2c093
guoxinming <guoxinming@zbj.com>
2021-01-20 19:23:47 +0800

    手写instanceof

--- JS/手写instanceOf/instanceOf.js
+++ JS/手写instanceOf/instanceOf.js
@@ -1,9 +1,16 @@
 /**
  * 实现instanceof功能
- *
+ * left instanceOf right
  * @param {*} left
  * @param {*} right
  */
 function instanceOf(left,right) {
-
+  let proto = left.__proto__;
+  let prototype = right.prototype;
+  while(true) {
+    // 找到最顶层
+    if(proto === null) return false;
+    if(proto === prototype) return true;
+    proto = proto.__proto__;
+  }
 }
