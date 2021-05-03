8707dcea5e96a0f9db995494540e244666867bac
guoxinming <guoxinming@zbj.com>
2021-05-03 18:43:52 +0800

    字符串模板实现

---
+++ JS/手写字符串模板/stringTemp.js
@@ -0,0 +1,17 @@
+function render(template, data) {
+  const reg = /\$\{(\w+)\}/;
+  if(reg.test(template)){
+    const name = reg.exec(template)[1];
+    template = template.replace(reg, data[name]);
+    return render(template, data);// 递归查找字符串模板变量，并渲染
+  }
+  return template;// 如果没有字符串模板，直接返回
+}
+
+// Test
+let template = '我是${name}，年龄${age}，性别${sex}';
+let person = {
+    name: '布兰',
+    age: 12
+}
+render(template, person); // output：我是布兰，年龄12，性别undefined
