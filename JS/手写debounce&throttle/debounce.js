060c0544f169c50f65110bcc8bedc908e2b4ab67
guoxinming <18883876325@163.com>
2023-03-29 14:47:39 +0800

    防抖节流

--- JS/手写debounce&throttle/debounce.js
+++ JS/手写debounce&throttle/debounce.js
@@ -1,8 +1,9 @@
+// 防抖：动作发生n秒后触发（延后执行），如果n秒内再次触发动作，重新计时
 function debounce(fn, timing=500) {
   let timer = null;
   return function() {
-    clearTimeout(timer)
-    timer = setTimeout(()=>{
+    clearTimeout(timer);
+    timer = setTimeout(() => {
       fn.apply(this, arguments)
     }, timing)
   }
--- JS/手写debounce&throttle/throttle.js
+++ JS/手写debounce&throttle/throttle.js
@@ -1,3 +1,4 @@
+// 节流：指定时间内只触发1次动作，若n秒内发生动作，无视动作。
 function throttle(fn, timing) {
   // 上次执行fn的时间戳
   let previous = 0;
@@ -5,7 +6,7 @@ function throttle(fn, timing) {
     let now = +new Date();
     if(now - previous > timing) {
       previous = now;
-      fn.apply(this, arguments)
+      fn.apply(this, arguments);
     }
   }
 }
