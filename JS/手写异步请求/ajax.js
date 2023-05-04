c8eecee93739edeb0bdc6a146d3c7fcbcb5c5d76
guoxinming <18883876325@163.com>
2023-05-04 21:43:47 +0800

    异步请求-错误监听

--- JS/手写异步请求/ajax.js
+++ JS/手写异步请求/ajax.js
@@ -6,7 +6,7 @@ const myAjax = url => {
     return new Promise((resolve, reject) => {
         const xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
         xhr.open('GET', url, false);
-        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
         xhr.onreadystatechange = () => {
             if(xhr.readyState !== 4) return;
             if(xhr.status >= 200 && xhr.status < 300) {
@@ -15,6 +15,10 @@ const myAjax = url => {
             }else{
                 reject(new Error(xhr.responseText));
             }
+        }
+        // 错误监听
+        xhr.onerror = () => {
+            reject(new Error(`Network Error`));
         }
         xhr.send();
     })
--- JS/手写异步请求/jsonp.js
+++ JS/手写异步请求/jsonp.js
@@ -1,9 +1,8 @@
 /**
  * jsonp 原理：script标签不受同源策略限制。
- * 1.动态创建 script 标签，其 src 指向非同源的 url，并传递callback参数给服务器，
- * 2.服务器返回以callback命名的函数调用和一些列参数，
+ * 1.动态创建 script 标签，其 src 指向非同源的 url，并传递callback参数给服务器;
+ * 2.服务器返回以callback命名的函数调用和一些列参数;
  * 3.页面收到响应后执行回调。
- *  
  */
 const jsonp = ({url, params, callbackName})=> {
     const generateUrl = () => {
@@ -23,6 +22,10 @@ const jsonp = ({url, params, callbackName})=> {
         window[callbackName] = data => {
             resolve(data)
             document.removeChild(scriptEle)
+        }
+        // 错误监听
+        scriptEle.onerror = err => {
+            reject(err)
         }
     })
 }
