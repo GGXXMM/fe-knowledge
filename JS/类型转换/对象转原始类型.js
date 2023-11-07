936f4df89e027c4ae14dbd3bdb75bbb0c26996c5
guoxinming <18883876325@163.com>
2023-11-07 11:55:51 +0800

    update[Symbol.toPrimitive]

--- JS/类型转换/对象转原始类型.js
+++ JS/类型转换/对象转原始类型.js
@@ -6,11 +6,18 @@ var obj = {
   toString() {
     return 'string'
   },
-  // [Symbol.toPrimitive]() {
-  //   return 3
+  // [Symbol.toPrimitive](hint) {
+  //   if(hint === 'number') {
+  //     return 3
+  //   }
+  //   if(hint === 'string') {
+  //     return 'toPrimitive'
+  //   } 
+  //   return 'default'
   // }
 }
-// 优先调 Symbol.toPrimitive（该优先级最高）
+/** Symbol.toPrimitive存在（该优先级最高） */ 
+/** Symbol.toPrimitive不存在： */
 console.log(+obj)// 倾向转number，调valueOf
 console.log(`${obj}`)// 倾向转字符串，调toString
-console.log(obj + {})// 默认default，先调valueOf，若无结果再调toString
+console.log(obj + {})// 默认default，优先调valueOf，再调toString
